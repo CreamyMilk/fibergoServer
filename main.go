ea4f74d6214dfc1f89cbe4ac08c61d2d57223d67
CreamyMilk <creamym254@gmail.com>
2021-01-23 15:13:48 +0300

    🔏 autocert

--- main.go
+++ main.go
@@ -9,6 +9,7 @@ import (
 	"github.com/gofiber/fiber/v2/middleware/cors"
 	"github.com/gofiber/fiber/v2/middleware/logger"
 	"github.com/gofiber/fiber/v2/middleware/recover"
+	"golang.org/x/crypto/acme/autocert"
 
 	"jotham/database"
 	"jotham/handler"
@@ -42,19 +43,37 @@ func main() {
 
 	app.Use(handler.NofileHandler)
 
-	cer, err := tls.LoadX509KeyPair("certs/ssl.cert", "certs/ssl.key")
-	if err != nil {
-		log.Fatal(err)
+	// Let’s Encrypt has rate limits: https://letsencrypt.org/docs/rate-limits/
+	// It's recommended to use it's staging environment to test the code:
+	// https://letsencrypt.org/docs/staging-environment/
+
+	// Certificate manager
+	m := &autocert.Manager{
+		Prompt: autocert.AcceptTOS,
+		// Replace with your domain
+		HostPolicy: autocert.HostWhitelist("readzy.africa"),
+		// Folder to store the certificates
+		Cache: autocert.DirCache("./certs"),
 	}
 
-	config := &tls.Config{Certificates: []tls.Certificate{cer}}
-
-	// Create custom listener
-	ln, err := tls.Listen("tcp", ":3000", config)
+	// TLS Config
+	cfg := &tls.Config{
+		// Get Certificate from Let's Encrypt
+		GetCertificate: m.GetCertificate,
+		// By default NextProtos contains the "h2"
+		// This has to be removed since Fasthttp does not support HTTP/2
+		// Or it will cause a flood of PRI method logs
+		// http://webconcepts.info/concepts/http-method/PRI
+		NextProtos: []string{
+			"http/1.1", "acme-tls/1",
+		},
+	}
+	ln, err := tls.Listen("tcp", ":3000", cfg)
 	if err != nil {
 		panic(err)
 	}
 
+	// Start server
 	log.Fatal(app.Listener(ln))
 	log.Fatal(app.Listen(":3001"))
 }
